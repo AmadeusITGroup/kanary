682ed81ade32c5a39591f8b8d16d166dcedd40d4
cedric lamoriniere <cedric.lamoriniere@datadoghq.com>
2019-04-13 23:11:45 +0200

    fix make validate
    Signed-off-by: cedric lamoriniere <cedric.lamoriniere@datadoghq.com>

---
+++ pkg/config/const.go
@@ -0,0 +1,4 @@
+package config
+
+// KanaryStatusSubresourceDisabledEnvVar use to know if status subresource is disabled
+const KanaryStatusSubresourceDisabledEnvVar = "KANARY_STATUS_SUBRESOURCE_DISABLED"
