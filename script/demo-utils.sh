e5ac4e22e1a89bbb1530e4cd5ff667b9fdd1f462
cedric lamoriniere <cedric.lamoriniere@datadoghq.com>
2019-05-14 23:11:31 +0200

    demo-utils.sh compatibility with osx

--- script/demo-utils.sh
+++ script/demo-utils.sh
@@ -17,47 +17,54 @@ readonly  reset=$(tput sgr0)
 readonly  green=$(tput bold; tput setaf 2)
 readonly yellow=$(tput bold; tput setaf 3)
 readonly   blue=$(tput bold; tput setaf 6)
-readonly timeout=$(if [ "$(uname)" == "Darwin" ]; then echo "1"; else echo "0.1"; fi) 
 
 function desc() {
     maybe_first_prompt
-    echo "${blue}# ${*}${reset}"
+    echo "$blue# $@$reset"
     prompt
 }
 
 function prompt() {
-    echo -n "${yellow}\$ ${reset}"
+    echo -n "$yellow\$ $reset"
 }
 
 started=""
 function maybe_first_prompt() {
-    if [ -z "${started}" ]; then
+    if [ -z "$started" ]; then
         prompt
         started=true
     fi
 }
 
-function run() {
-    maybe_first_prompt
-    rate=60
-    if [ -n "${DEMO_RUN_FAST}" ]; then
-      rate=1000
-    fi
-    echo "${green}${1}${reset}" | pv -qL ${rate}
-    if [ -n "${DEMO_RUN_FAST}" ]; then
-      sleep 0.5
-    fi
-    OFILE="$(mktemp -t "$(basename "${0}").XXXXXX")"
-    script -eq -c "${1}" -f "${OFILE}"
-    r=${?}
-    read -r -d '' -t "${timeout}" -n 10000 # clear stdin
-    prompt
-    if [ -z "${DEMO_AUTO_RUN}" ]; then
-      read -r -s
-    fi
-    return ${r}
+function backtotop() {
+    
+    clear
 }
 
-#SSH_NODE=$(kubectl get nodes | tail -1 | cut -f1 -d' ')
+function run() {
+    maybe_first_prompt
+    rate=25
+    if [ -n "$DEMO_RUN_FAST" ]; then
+      rate=1000
+    fi
+    echo "$green$1$reset" | pv -qL $rate
+    if [ -n "$DEMO_RUN_FAST" ]; then
+      sleep 0.5
+    fi
+    eval "$1"
+    r=$?
+    read -d '' -t 1 -n 10000 # clear stdin
+    prompt
+    if [ -z "$DEMO_AUTO_RUN" ]; then
+      read -s
+    fi
+    return $r
+}
+
+function relative() {
+    for arg; do
+        echo "$(realpath $(dirname $(which $0)))/$arg" | sed "s|$(realpath $(pwd))|.|"
+    done
+}
 
 trap "echo" EXIT
