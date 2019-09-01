9afe0888b123bfd692da97b61a69dc4313c49576
Augustin Husson <4548045+Nexucis@users.noreply.github.com>
2019-09-01 16:42:00 +0200

    fix building operateor-sdk (#41)

--- hack/install-operator-sdk.sh
+++ hack/install-operator-sdk.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
-go get -u github.com/golang/dep/cmd/dep
 mkdir -p $GOPATH/src/github.com/operator-framework
 cd $GOPATH/src/github.com/operator-framework
 git clone https://github.com/operator-framework/operator-sdk
 cd operator-sdk
 git checkout master
-make dep
 make install
